e2f6eb7e1a1e4b439f74257ccb849783bbd1a830
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-07-26 17:09:33 +0530

    feat: Add reclaimTokens function for refreshing access tokens

--- genrateToken/src/controller/token.controller.js
+++ genrateToken/src/controller/token.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import redis from "../utils/redisClient.js";
+import mongoose from "mongoose";
 
 const genratetokens = asyncHandler(async (req,res) => {
     try {
@@ -41,4 +42,36 @@ const genratetokens = asyncHandler(async (req,res) => {
     }
 })
 
-export {genratetokens}
+const reclaimTokens = asyncHandler(async (req,res) => {
+    const refreshToken = req.user.refreshToken
+    if (! refreshToken){
+        throw new ApiError(400, "Refresh Token is not in user request")
+    }
+    const user = await mongoose.connection.db.collection("users").findOne({
+        refreshToken: refreshToken
+    });
+    if (user._id!==req.user._id) {
+        throw new ApiError(400, "Something fishy token not found for logged in user")
+    }
+    if (!user) {
+        throw new ApiError(400, "Refresh Token not found in user collection");
+    }
+    const accessToken = jwt.sign(
+        {
+            _id: req.user._id,
+            email: req.user.email,
+            username: req.user.username,
+            fullName: req.user.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+    return res.status(200).json(new ApiResponse (200, {accessToken}, "Access Token Refreshed"))
+})
+
+export {
+    genratetokens
+    ,reclaimTokens
+}
--- genrateToken/src/routes/token.route.js
+++ genrateToken/src/routes/token.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { genratetokens } from "../controller/token.controller.js";
+import { genratetokens, reclaimTokens } from "../controller/token.controller.js";
 import { checkLoginService } from "../middleware/onlyLoginAllow.middleware.js";
 const router = Router()
 
 router.route("/token-generate").post(checkLoginService, genratetokens)
+router.route("refreshAccessToken").get(reclaimTokens)
 export  default router 
