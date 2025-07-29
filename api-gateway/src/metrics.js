11b35fa9b940ce62c3bca9a7d1f7538b40f1a03c
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-07-29 18:53:45 +0530

    feat(api-gateway): implement Prometheus metrics and middleware
    - Added Express server setup in app.js with CORS and JSON body parsing.
    - Integrated Prometheus metrics collection in metrics.js, including HTTP request counter and duration histogram.
    - Created a middleware in prom.middleware.js to track request metrics.
    - Exposed a /metrics endpoint to serve Prometheus metrics.

---
+++ api-gateway/src/metrics.js
@@ -0,0 +1,22 @@
+import client from "prom-client";
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+// HTTP Request Counter
+export const httpRequestCounter = new client.Counter({
+  name: "talkz_loginService_http_requests_total",
+  help: "Total HTTP requests",
+  labelNames: ["method", "route", "status"],
+});
+register.registerMetric(httpRequestCounter);
+
+export const httpRequestDurationHistogram = new client.Histogram({
+  name: "talkz_loginService_http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status"],
+  buckets: [0.005, 0.01, 0.05, 0.1, 0.3, 0.5, 1, 2, 5] // Adjust based on app latency
+});
+register.registerMetric(httpRequestDurationHistogram);
+
+export { register }
