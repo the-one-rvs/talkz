63fcbc857e3b4cce1cb2e5e469d2785033ba22fa
the-one-rvs <vaibhavsarswat142005@gmail.com>
2026-02-13 21:45:33 +0530

    fix: Include additional socket environment variables in Nginx config substitution

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Replace env vars in Nginx config
-envsubst '$API_HOST $API_PORT' '$SOCKET_HOST' '$SOCKET_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$API_HOST $API_PORT $SOCKET_HOST $SOCKET_PORT' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
 
-# Execute CMD (nginx)
 exec "$@"
